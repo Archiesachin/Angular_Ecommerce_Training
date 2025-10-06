ae9d32a6631151151a23beb148f6b36d9482592e
Archiesachin <archieshah8767@gmail.com>
2025-10-06 18:20:56 +0530

    navbar added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { ContactUs } from './pages/contact-us/contact-us';
+import { Cart } from './pages/cart/cart';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:Home}, 
+    {path:"contact-us", component:ContactUs},
+    {path:"cart", component:Cart},
+
+];
